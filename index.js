f76738bbc3e59711e66e44e29b6d6fa911247d8e
Terrance <terrancekoar@gmail.com>
2017-06-11 21:44:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = "I don't do much."
+var dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = '#FF69B4'
+dodger.style.left
+dodger.style.bottom = '0px'
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  } else if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left => 10) {
+    console.log(left)
+    dodger.style.left = `${left - 5}px`
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left + 5}px`
+  }
+}
